6cea197d220f80454838189d5e27e8c22d70b9b4
hasan <36960633+hasanshofan@users.noreply.github.com>
2022-09-25 18:15:15 +0300

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -6,6 +6,7 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+        <meta name="google-site-verification" content="-dHMl6XhbXNj_jQqEPgUeHbmA0UyiizEUfwGtyhewOU" />
     </Head>
   );
 }
